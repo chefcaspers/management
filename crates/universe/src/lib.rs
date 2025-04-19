0a79fe4c4fe3abf16548b3d11fa735b38c91c136
Robert Pack <robstar.pack@gmail.com>
2025-04-19 19:59:34 +0200

    feat: create people

--- crates/universe/src/lib.rs
+++ crates/universe/src/lib.rs
@@ -2,8 +2,31 @@ mod agents;
 mod error;
 mod idents;
 mod models;
+mod positions;
 mod simulation;
 
 pub use self::agents::*;
 pub use self::simulation::state;
 pub use self::simulation::{Simulatable, Simulation};
+
+#[cfg(test)]
+mod tests {
+    use arrow_cast::pretty::print_columns;
+    use geoarrow_geoparquet::GeoParquetRecordBatchReaderBuilder;
+
+    #[test]
+    fn test_simulation() {
+        let file = std::fs::File::open("../../data/georef-united-states-of-america-county.parquet")
+            .unwrap();
+
+        let mut asd = GeoParquetRecordBatchReaderBuilder::try_new(file)
+            .unwrap()
+            .build()
+            .unwrap();
+        let batch = asd.next().unwrap().unwrap();
+        //let asd = read_geojson(BufReader::new(file), Some(1024)).unwrap();
+        let arr = batch.column_by_name("ste_name").unwrap();
+        print_columns("counties", &[arr.clone()]).unwrap();
+        // print!("{:?};", batch.schema());
+    }
+}
