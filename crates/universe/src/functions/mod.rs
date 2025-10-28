ae589dc88dc237c4785b8a1a961ed800cadeb8a7
Robert Pack <robstar.pack@gmail.com>
2025-10-28 14:38:00 +0100

    feat: add h3_longlatash3 scalar udf

---
+++ crates/universe/src/functions/mod.rs
@@ -0,0 +1,24 @@
+use datafusion::logical_expr::ScalarUDF;
+
+mod h3;
+
+#[macro_export]
+macro_rules! make_udf_function {
+    ($UDF:ty, $NAME:ident) => {
+        #[allow(rustdoc::redundant_explicit_links)]
+        #[doc = concat!("Return a [`ScalarUDF`](datafusion_expr::ScalarUDF) implementation of ", stringify!($NAME))]
+        pub fn $NAME() -> std::sync::Arc<ScalarUDF> {
+            // Singleton instance of the function
+            static INSTANCE: std::sync::LazyLock<
+                std::sync::Arc<ScalarUDF>,
+            > = std::sync::LazyLock::new(|| {
+                std::sync::Arc::new(ScalarUDF::new_from_impl(
+                    <$UDF>::new(),
+                ))
+            });
+            std::sync::Arc::clone(&INSTANCE)
+        }
+    };
+}
+
+make_udf_function!(h3::LongLatAsH3, h3_longlatash3);
